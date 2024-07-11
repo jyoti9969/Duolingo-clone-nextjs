62b883b91e2b22c98dbfa52050975762be44b5e3
Jyoti <jyotigupta282002@gmail.com>
2024-07-11 14:12:13 +0530

    last commit

--- db/queries.ts
+++ db/queries.ts
@@ -73,10 +73,21 @@ return data
 })
 
 export const getCoursesById=cache(async(courseId:number)=>{
-    const data= await db.query.courses.findFirst({
-        where: eq(courses.id,courseId)
-    })
-    return data
+    const data = await db.query.courses.findFirst({
+        where: eq(courses.id, courseId),
+        with: {
+          units: {
+            orderBy: (units, { asc }) => [asc(units.order)],
+            with: {
+              lessons: {
+                orderBy: (lessons, { asc }) => [asc(lessons.order)],
+              },
+            },
+          },
+        },
+       
+      });
+      return data
 })
 export const getCourseProgress=cache(async()=>{
     const {userId}=await auth()
